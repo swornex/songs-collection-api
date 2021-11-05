8b7760660a5801109a74c187ffd7c0f959534262
Swornima <sworstha@gmail.com>
2021-11-05 22:15:26 +0545

    completed C and R of CRUD for users

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const userRouter = require("./routers/user");
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(userRouter);
 
 mongoose
     .connect("mongodb://localhost:27017/song-collection")
--- routers/user.js
+++ routers/user.js
@@ -1,2 +1,38 @@
 const { Router } = require("express");
 const User = require("../models/userModel");
+
+const userRouter = Router();
+
+userRouter.post("/users", async (request, response) => {
+    const { name, email, password } = request.body;
+    const user = new User({ name, email, password });
+    try {
+        await user.save();
+        response.status(201).send(user);
+    } catch (e) {
+        response.status(400).send({ error: e.message });
+    }
+});
+
+userRouter.get("/users", async (request, response) => {
+    try {
+        const user = await User.find();
+        response.send(user);
+    } catch (e) {
+        response.status(500).send({ error: e.message });
+    }
+});
+
+userRouter.get("/users/:id", async (request, response) => {
+    try {
+        const user = await User.findById(request.params.id);
+        if (!user) {
+            return response.status(404).send({ error: "User not found." });
+        }
+        response.send(user);
+    } catch (e) {
+        response.status(500).send({ error: e.message });
+    }
+});
+
+module.exports = userRouter;
